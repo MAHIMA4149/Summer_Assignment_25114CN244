f51e6f54f5f8317a52e20d827d595429b0cf42d1
MAHIMA4149 <mahimayadav99180@gmail.com>
2026-06-06 06:11:23 -0600

    DAY-6 (FINISHED)

---
+++ DAY-6/Code2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int binary, decimal = 0, rem, i = 0;
+    cout << "Enter a binary number: ";
+    cin >> binary;
+    while (binary != 0) {
+        rem = binary % 10;
+        decimal = decimal + rem * pow(2, i);
+        binary = binary / 10;
+        i++;
+    }
+    cout << "Decimal conversion = " << decimal;
+    return 0;
+}

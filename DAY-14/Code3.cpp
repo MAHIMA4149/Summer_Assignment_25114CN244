ace22b980788daa2555d08f8e981ffbcec0c41f4
MAHIMA4149 <mahimayadav99180@gmail.com>
2026-06-14 09:25:12 -0600

    DAY-14(FINISHED)

---
+++ DAY-14/Code3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int largest = INT_MIN, secondLargest = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    cout << "Second Largest Element = " << secondLargest;
+    return 0;
+}

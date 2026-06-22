340a38cecdfece6330eb240f5c5ae3c07464dba3
MAHIMA4149 <mahimayadav99180@gmail.com>
2026-06-22 10:26:22 -0600

    DAY- 22 (FINISHED)

---
+++ DAY-22/Code2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    int count = 1;
+
+    cout << "Enter a sentence: ";
+    getline(cin, str);
+
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == ' ')
+            count++;
+    }
+    cout << "Number of words = " << count;
+    return 0;
+}
